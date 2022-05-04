4c9320076b59144334f1d6dea226028f246197ae
24-002580\user <annarainchik@gmail.com>
2022-05-04 13:11:30 +0300

    add new 04.05

--- src/test/java/tests/PracticeForm.java
+++ src/test/java/tests/PracticeForm.java
@@ -2,14 +2,16 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
-public class PracticeForm {
+public class PracticeForm  extends TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
